07f089314f6465952d7bede12dde0ebf172af89d
Matías <saitam.spatz@gmail.com>
2016-06-15 19:35:18 +0200

    15.06.16

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key:         'aVRuUUgSjvFHmaF7XcW4zXDVs',
-  consumer_secret:      'kkH3ZPGx9tjQUBRwuiKi7egCAyBJgj1k97aMt0876tprZdcsnS',
+  consumer_key:         'opr3jjBxtU7wByiBJ4KdgtafK',
+  consumer_secret:      'WhfHDKocNkwpTDhqlFB0HJ6H2yHUS8YY06imYOGdKM9bEHGWbo',
   access_token:         '717112776787894272-ivIXd75xk32PnbVIgN5vkbtoKd2exEW',
   access_token_secret:  'ah7vB16GvgvAf5KK2ISwoBpz37mEwIuf8eqwnfOmUJD2X'
 }
