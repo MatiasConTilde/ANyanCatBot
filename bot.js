b9abb915dddf9c16cd58d238421773c0406744da
ANyanCatFan <saitam.spatz@gmail.com>
2017-03-05 14:22:26 +0100

    Change from worker to web; Single quotes to double quotes; var to const

--- bot.js
+++ bot.js
@@ -1,20 +1,18 @@
-console.log('The bot is starting');
+console.log("The bot is starting");
 
-var Twit = require('twit');
+const Twit = require("twit");
 
-var config = require('./config');
-var T = new Twit(config);
-var stream = T.stream('user');
+const T = new Twit(require("./config"));
+const stream = T.stream("user");
 
-stream.on('follow', function(eventMsg) {
+stream.on("follow", function(eventMsg) {
   console.log("Follow event!");
-  var name = eventMsg.source.name;
-  var screenName = eventMsg.source.screen_name;
-  T.post('statuses/update', {
-      status: '@' + screenName + ' hello!'
-    },
-    function(err, data, response) {
-      if (err) console.log("Something went wrong!");
-      else console.log("It worked!");
-    });
+  const name = eventMsg.source.name;
+  const screenName = eventMsg.source.screen_name;
+  T.post("statuses/update", {
+    status: "@" + screenName + " hello!"
+  }, function(err, data, response) {
+    if (err) console.log("Something went wrong!");
+    else console.log("It worked!");
+  });
 });
